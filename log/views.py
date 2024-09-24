5c2c7bd0364a8e661f0738466078a666b4960a3b
codewithchaos <codewithchaos1809@gmail.com>
2024-09-23 13:09:57 -1200

    Initial commit

---
+++ log/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+
+def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse("Invalid credentials")
+    return render(request, 'log/login.html')
+
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                return HttpResponse("Username already exists")
+            elif User.objects.filter(email=email).exists():
+                return HttpResponse("Email already exists")
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('login')
+        else:
+            return HttpResponse("Passwords do not match")
+    return render(request, 'log/signup.html')
+
+def index(request):
+    return render(request, 'log/index.html')
